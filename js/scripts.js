53e3701a5485488313ac1fbfb0b6f3fbe39a3ce6
Sean Mussack <seanmussack@gmail.com>
2021-01-10 01:24:34 -0600

    Added an info-accordion to dinosaurs.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ $(document).ready(function(){
     $(".infoAccordion a").click(function(){
         $(".infoAccordion a").find(".fa-minus").addClass("fa-plus");
         $(".infoAccordion a").find(".fa-minus").removeClass("fa-minus");
+        $(".infoAccordion a").find(".fa-minus-square-o").addClass("fa-plus-square-o");
+        $(".infoAccordion a").find(".fa-minus-square-o").removeClass("fa-plus-minus-o");
         if (!($(this).find("div").hasClass("show"))){
             $(this).find(".fa-plus").addClass("fa-minus");
             $(this).find(".fa-plus").removeClass("fa-plus");
+            $(this).find(".fa-plus-square-o").addClass("fa-minus-square-o");
+            $(this).find(".fa-plus-square-o").removeClass("fa-plus-square-o");
         }
     });
     $("#newsletterModal").on("shown.bs.modal", function(){
